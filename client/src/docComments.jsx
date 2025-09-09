ab4003d700eb00687b69576046e42cbaa0b7ae57
NAKANO.M <114970041+stumn@users.noreply.github.com>
2025-09-09 14:53:20 +0900

    DocComments名前変更

--- client/src/docComments.jsx
+++ client/src/docComments.jsx
@@ -1,4 +1,4 @@
-// // File: client/src/DocComments.jsx
+// // File: client/src/DocComments.jsx 
 
 import React, { useEffect, useRef, useMemo, useState } from 'react';
 import { VariableSizeList as List } from 'react-window';
